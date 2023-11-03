35730c2609c913181c97049510f94c919d61c63d
GC101st <58160013+GC101st@users.noreply.github.com>
2023-11-02 19:47:36 -0500

    Update server.lua

--- CustomProp/server.lua
+++ CustomProp/server.lua
@@ -1,3 +1,3 @@
 local versioner = exports['sb-versioner'].initiate()
-local repo = 'https://github.com/BryceCanyonCounty/bcc-anticheat'
+local repo = 'https://github.com/GC101st/CustomProp/tree/main/CustomProp'
 versioner.checkFile(GetCurrentResourceName(), repo)
